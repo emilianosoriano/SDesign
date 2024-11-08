f5ba055c21641d7f00ef056ffecd855c12353bc7
Rene <rener2104@gmail.com>
2024-11-08 11:32:53 -0600

    Add firebase authentication for login/signup. Pending interface for logging in and registering.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,18 +1,32 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { auth } from '../Firebase';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
 import '../styles/Home.css'; // Add a CSS file for styling
 
 const Home = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isSigningup, setIsSigningup] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
 
-  const handleLogin = () => {
-    // Handle login logic
-    setIsLoggedIn(true);
+  const handleLogin = async () => {
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      setIsLoggedIn(true);
+    } catch (error) {
+      setError('Failed to log in');
+    }
   };
 
-  const handleSignup = () => {
-    // Handle signup logic
-    setIsLoggedIn(true);
+  const handleSignup = async () => {
+    try {
+      await createUserWithEmailAndPassword(auth, email, password);
+      setIsLoggedIn(true);
+    } catch (error) {
+      setError('Signup failed');
+    }
   };
 
   return (
